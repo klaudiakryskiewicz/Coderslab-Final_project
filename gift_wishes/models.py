b3dfe768d0dd95de25736813b65c704eab90c59c
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-06-22 16:20:35 +0200

    Models

--- gift_wishes/models.py
+++ gift_wishes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Family(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class Member(models.Model):
+    genders = [('F', 'Kobieta'), ('M', 'Mężczyzna'), ('None', '-')]
+    name = models.CharField(max_length=120)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=9, choices=genders, default='None')
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Wish(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    link = models.CharField(max_length=300)
+    price = models.IntegerField()
+    Member = models.ManyToManyField(Member)
+
+
+class Present(models.Model):
+    wish = models.OneToOneField(Wish, on_delete=models.CASCADE)
+    is_bought = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
